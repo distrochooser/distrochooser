84720b1d71371e0f1a472484bf1e09036cc5ebb8
squarerootfury <fury224@googlemail.com>
2017-04-08 11:16:04 +0200

    + Minified as much as I can

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,14 @@ var thirdparty = [
     "./3rdparty/font-awesome.min.css",
     "./3rdparty/jquery.rateyo.min.css",
 ];
+var thirdPartyScripts = [
+    "./3rdparty/jquery-2.2.4.min.js",
+    "./3rdparty/bootstrap.min.js",
+    "./3rdparty/jquery.rateyo.min.js",
+    "./3rdparty/vue.min.js",
+    "./3rdparty/Chart.min.js",
+    "./3rdparty/vue-resource.min.js"
+];
 
 var getCopyright = function () {
     return fs.readFileSync('./header.txt');
@@ -38,6 +46,11 @@ gulp.task('build', function () {
      gulp.src(thirdparty) 
     .pipe(minifyCss())
     .pipe(concat('3rdparty.min.css'))
+    .pipe(gulp.dest('built'));
+    gulp.src(thirdPartyScripts) 
+    .pipe(uglify())
+    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+    .pipe(concat('3rdparty.min.js'))
     .pipe(gulp.dest('built'));
 });
 
