2a6d05f6a2f4ed0d2d53d4695c3417bd14f60676
cmllr <fury224@googlemail.com>
2019-06-07 09:52:30 +0200

    Removed log

--- viisi/mixins/i18n.js
+++ viisi/mixins/i18n.js
@@ -1,12 +1,6 @@
 export default {
   methods: {
     __i: function(val) {
-      if (
-        this.$store.state.translations === null ||
-        typeof this.$store.state.translations[val] === 'undefined'
-      ) {
-        console.log(val)
-      }
       return this.$store.state.translations !== null &&
         typeof this.$store.state.translations[val] !== 'undefined'
         ? this.$store.state.translations[val]
