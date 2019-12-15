76c79823acb320fcaf18a8f210a59158ff95a276
cmllr <fury224@googlemail.com>
2019-12-15 14:25:11 +0100

    Added some performance traps. Gained ~ 0.5 s per submitAnswer() request

--- backend/distrochooser/calculations/refactored.py
+++ backend/distrochooser/calculations/refactored.py
@@ -5,26 +5,27 @@ from django.forms.models import model_to_dict
 def saveAnswers(userSession, rawAnswers):
   # Delete old answers
   GivenAnswer.objects.filter(session=userSession).delete()
+  allAnswers =  Answer.objects.all()
   for answer in rawAnswers:
     givenAnswer = GivenAnswer()
     givenAnswer.session = userSession
-    givenAnswer.answer = Answer.objects.get(msgid=answer['msgid'])
+    givenAnswer.answer = allAnswers.get(msgid=answer['msgid'])
     givenAnswer.isImportant = answer['important']
     givenAnswer.save()
 
+from django.db import connection
+from silk.profiling.profiler import silk_profile
+@silk_profile()
 def getSelections(userSession, data, langCode):
   translationToUse = TRANSLATIONS[langCode] if langCode in TRANSLATIONS else TRANSLATIONS["en"]
   ResultDistroSelection.objects.filter(session=userSession).delete()
-  userSession.checksDone = 0
-  userSession.save()
   saveAnswers(userSession, data['answers'])
-  givenAnswers = GivenAnswer.objects.filter(session=userSession).values("answer","isImportant")
+  givenAnswers = GivenAnswer.objects.filter(session=userSession).prefetch_related('answer').values("answer","isImportant")
   importantAnswers = list(map(lambda o: o["answer"], filter(lambda o: o["isImportant"], givenAnswers)))
   distros = Distribution.objects.all()
-  matrixTuples = AnswerDistributionMatrix.objects.all()
-  matchingTuples = matrixTuples.filter(answer_id__in=(o["answer"] for o in givenAnswers))
+  matchingTuples = AnswerDistributionMatrix.objects.all().prefetch_related('distros', 'answer')
   distroReasons = {}
-  createdSelections = []
+  createdSelections = {}
   reasonsBySelection = {}
 
   for distro in distros:
@@ -32,12 +33,15 @@ def getSelections(userSession, data, langCode):
     selection.session = userSession
     selection.distro = distro
     selection.save()
-    createdSelections.append(selection)
+    createdSelections[distro.id] = selection
     reasonsBySelection[selection.id] = []
   
   for matrixTuple in matchingTuples:
-    userSession.checksDone = userSession.checksDone +1
-    userSession.save()
+    if matrixTuple.answer not in (o["answer"] for o in givenAnswers):
+      pass
+    selectedDescription = translationToUse[matrixTuple.description] if matrixTuple.description in translationToUse else matrixTuple.description 
+
+
     reason = SelectionReason()
     reason.isImportant = matrixTuple.answer.pk in importantAnswers
     reason.resultSelection = None
@@ -45,31 +49,27 @@ def getSelections(userSession, data, langCode):
     reason.isBlockingHit = matrixTuple.isBlockingHit
     reason.isPositiveHit = not matrixTuple.isNegativeHit
     reason.isNeutralHit = matrixTuple.isNeutralHit
-    reason.description = translationToUse[matrixTuple.description] if matrixTuple.description in translationToUse else matrixTuple.description 
+    reason.description = selectedDescription
 
     if reason.isNeutralHit:
       reason.isPositiveHit = True
+      
     for distro in matrixTuple.distros.all():
-      selection = None
-      suitableSelections = list(filter(lambda o: o.distro == distro, createdSelections))
-      if len(suitableSelections) != 1:
-        raise Exception("Invalid state")
-      else:
-        selection = suitableSelections[0]
+      selection = createdSelections[distro.id]
       # prevent that same descritptions appear multiple times
       isDescriptionAlreadyInReasonList = len(list(filter(lambda r: r.description == reason.description,reasonsBySelection[selection.id]))) > 0
       if not isDescriptionAlreadyInReasonList:
         reason.resultSelection = selection
-        reason.save()
         reasonsBySelection[selection.id].append(reason)
 
   results = []
-  for selection in createdSelections:
+  for _, selection in createdSelections.items():
     reasons = reasonsBySelection[selection.id]
+    SelectionReason.objects.bulk_create(reasons)
     results.append(
       {
-        "distro": model_to_dict(selection.distro, exclude="logo"),
-        "reasons": list(map(lambda r: model_to_dict(r), reasons)),
+        "distro": model_to_dict(selection.distro, exclude=["logo", "id"]),
+        "reasons": list(map(lambda r: model_to_dict(r,exclude=["id", "resultSelection"]), reasons)),
         "selection": selection.id
       }
     )
