b0ba3224670d7e6a903ca8a24c9a3938cce7f115
cmllr <fury224@googlemail.com>
2019-12-24 16:43:39 +0100

    Added some slight changes to avoid often save() calls

--- backend/distrochooser/calculations/refactored.py
+++ backend/distrochooser/calculations/refactored.py
@@ -5,66 +5,74 @@ from django.forms.models import model_to_dict
 def saveAnswers(userSession, rawAnswers):
   # Delete old answers
   GivenAnswer.objects.filter(session=userSession).delete()
-  allAnswers =  Answer.objects.all()
+  newAnswers = []
+  selectedAnswers = {answer[0]: answer[1] for answer in Answer.objects.filter(msgid__in=[a["msgid"] for a in rawAnswers]).values_list('msgid', 'pk')}
   for answer in rawAnswers:
-    givenAnswer = GivenAnswer()
-    givenAnswer.session = userSession
-    givenAnswer.answer = allAnswers.get(msgid=answer['msgid'])
-    givenAnswer.isImportant = answer['important']
-    givenAnswer.save()
+    newAnswers.append(
+      GivenAnswer(
+          session=userSession,
+          answer_id=selectedAnswers[answer['msgid']],
+          isImportant=answer['important']
+      )
+    )
+  GivenAnswer.objects.bulk_create(newAnswers)
 
-from django.db import connection
-from silk.profiling.profiler import silk_profile
-@silk_profile()
 def getSelections(userSession, data, langCode):
   translationToUse = TRANSLATIONS[langCode] if langCode in TRANSLATIONS else TRANSLATIONS["en"]
   ResultDistroSelection.objects.filter(session=userSession).delete()
   saveAnswers(userSession, data['answers'])
+
   givenAnswers = GivenAnswer.objects.filter(session=userSession).prefetch_related('answer').values("answer","isImportant")
   importantAnswers = list(map(lambda o: o["answer"], filter(lambda o: o["isImportant"], givenAnswers)))
   distros = Distribution.objects.all()
   matchingTuples = AnswerDistributionMatrix.objects.all().prefetch_related('distros', 'answer')
-  distroReasons = {}
-  createdSelections = {}
-  reasonsBySelection = {}
 
+
+  createdSelections = {}
+  createdReasons = {}
+
+
+  newSelections = []
   for distro in distros:
     selection = ResultDistroSelection()
     selection.session = userSession
     selection.distro = distro
-    selection.save()
+    newSelections.append(selection)
     createdSelections[distro.id] = selection
-    reasonsBySelection[selection.id] = []
+    createdReasons[distro.id] = []
   
+
   for matrixTuple in matchingTuples:
-    if matrixTuple.answer not in (o["answer"] for o in givenAnswers):
-      pass
-    selectedDescription = translationToUse[matrixTuple.description] if matrixTuple.description in translationToUse else matrixTuple.description 
+    isInAnswerList = matrixTuple.answer.pk in (o["answer"] for o in givenAnswers)
+    if isInAnswerList:
+      selectedDescription = translationToUse[matrixTuple.description] if matrixTuple.description in translationToUse else matrixTuple.description 
 
 
-    reason = SelectionReason()
-    reason.isImportant = matrixTuple.answer.pk in importantAnswers
-    reason.resultSelection = None
+      reason = SelectionReason()
+      reason.isImportant = matrixTuple.answer.pk in importantAnswers
+      reason.resultSelection = None
 
-    reason.isBlockingHit = matrixTuple.isBlockingHit
-    reason.isPositiveHit = not matrixTuple.isNegativeHit
-    reason.isNeutralHit = matrixTuple.isNeutralHit
-    reason.description = selectedDescription
+      reason.isBlockingHit = matrixTuple.isBlockingHit
+      reason.isPositiveHit = not matrixTuple.isNegativeHit
+      reason.isNeutralHit = matrixTuple.isNeutralHit
+      reason.description = selectedDescription
 
-    if reason.isNeutralHit:
-      reason.isPositiveHit = True
-      
-    for distro in matrixTuple.distros.all():
-      selection = createdSelections[distro.id]
-      # prevent that same descritptions appear multiple times
-      isDescriptionAlreadyInReasonList = len(list(filter(lambda r: r.description == reason.description,reasonsBySelection[selection.id]))) > 0
-      if not isDescriptionAlreadyInReasonList:
-        reason.resultSelection = selection
-        reasonsBySelection[selection.id].append(reason)
+      if reason.isNeutralHit:
+        reason.isPositiveHit = True
+        
+      for distro in matrixTuple.distros.all():
+        selection = list(filter(lambda s: s.distro == distro, newSelections))[0]
+        if selection.pk is None:
+          selection.save()
+        # prevent that same descritptions appear multiple times
+        isDescriptionAlreadyInReasonList = len(list(filter(lambda r: r.description == reason.description, createdReasons[distro.id]))) > 0
+        if not isDescriptionAlreadyInReasonList:
+          reason.resultSelection = selection
+          createdReasons[distro.id].append(reason)
 
   results = []
-  for _, selection in createdSelections.items():
-    reasons = reasonsBySelection[selection.id]
+  for distroId, selection in createdSelections.items():
+    reasons = createdReasons[distroId]
     SelectionReason.objects.bulk_create(reasons)
     results.append(
       {
