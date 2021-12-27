21bbd45b1648b9c53f8504fa3a96d1b971193283
cmllr <fury224@googlemail.com>
2021-12-27 16:59:06 +0100

    Update dependencies

--- viisi/nuxt.config.js
+++ viisi/nuxt.config.js
@@ -38,24 +38,4 @@ module.exports = {
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
-
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
 }
