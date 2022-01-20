fe43e57478f02e63d575fe5d8cc299155a28e963
trickypr <trickypr@icloud.com>
2022-01-20 15:16:27 +1100

    🔧 Tell nuxt to stop bothering me about telemetry

--- viisi/nuxt.config.js
+++ viisi/nuxt.config.js
@@ -1,40 +1,47 @@
 const pkg = require('./package')
 
 module.exports = {
+  telemetry: true,
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    meta: [{hid: "google-site-verification", name: "google-site-verification", content:"nqtoKAtXX7xTNyddaEGkkYtgpc0pc0b-wigel0Acy5c"}],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.svg' }]
+    meta: [
+      {
+        hid: 'google-site-verification',
+        name: 'google-site-verification',
+        content: 'nqtoKAtXX7xTNyddaEGkkYtgpc0pc0b-wigel0Acy5c',
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.svg' }],
   },
 
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#fff' },
 
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [],
 
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
   ],
   /*
-  ** Axios module configuration
-  */
+   ** Axios module configuration
+   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
