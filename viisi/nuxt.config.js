02ced5da13f6aafe915aca575785020daa848ad5
cmllr <mail@chmr.eu>
2022-09-25 12:25:11 +0200

    Add sitemap

--- viisi/nuxt.config.js
+++ viisi/nuxt.config.js
@@ -30,7 +30,8 @@ module.exports = {
   */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    "@nuxtjs/sitemap",
   ],
   /*
   ** Axios module configuration
@@ -38,4 +39,45 @@ module.exports = {
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
+  sitemap: {
+    hostname: 'https://distrochooser.de',
+    gzip: true,
+    exclude: [
+      '/info/privacy/*',
+      '/info/imprint/*'
+    ],
+    routes: [
+      '/',
+      '/de',
+      '/en',
+      '/es',
+      '/fi',
+      '/fr',
+      '/ch',
+      '/he',
+      '/it',
+      '/nl',
+      '/pt-br',
+      '/ru',
+      '/tr',
+      '/vn',
+      '/zh-hans',
+      '/zh-hant',
+      '/info/about/de',
+      '/info/about/en',
+      '/info/about/es',
+      '/info/about/fi',
+      '/info/about/fr',
+      '/info/about/ch',
+      '/info/about/he',
+      '/info/about/it',
+      '/info/about/nl',
+      '/info/about/pt-br',
+      '/info/about/ru',
+      '/info/about/tr',
+      '/info/about/vn',
+      '/info/about/zh-hans',
+      '/info/about/zh-hant',
+    ]
+  }
 }
