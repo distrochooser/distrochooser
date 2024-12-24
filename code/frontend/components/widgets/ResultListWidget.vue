2abf50b10669e7256907266d487cbc885f1c5d27
cmllr <mail@chmr.eu>
2024-12-24 15:49:19 +0100

    Add serialization for result lists

--- code/frontend/components/widgets/ResultListWidget.vue
+++ code/frontend/components/widgets/ResultListWidget.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         RESULT LIST
+
+        <div v-for="(choosable, index) in props.widget.choosables.sort((a,b) => a.rank - b.rank)" :key="index">
+          {{  choosable  }}
+        </div>
     </div>
 </template>
 <script setup lang="ts">
