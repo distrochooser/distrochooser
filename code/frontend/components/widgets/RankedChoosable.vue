b730257999abb43d086bd61ecf4f8f5e14e4879e
cmllr <mail@chmr.eu>
2024-12-24 15:53:14 +0100

    Render Choosable

---
+++ code/frontend/components/widgets/RankedChoosable.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{ props.choosable.name }}
+        {{ props.choosable.meta }}
+    </div>
+</template>
+<script setup lang="ts">
+import type {  RankedChoosable } from '~/sdk';
+
+
+
+interface WidgetProps {
+  choosable: RankedChoosable;
+}
+
+const props = defineProps<WidgetProps>();
+</script>
--- code/frontend/components/widgets/ResultListWidget.vue
+++ code/frontend/components/widgets/ResultListWidget.vue
@@ -3,12 +3,13 @@
         RESULT LIST
 
         <div v-for="(choosable, index) in props.widget.choosables.sort((a,b) => a.rank - b.rank)" :key="index">
-          {{  choosable  }}
+          <RankedChoosable :choosable="choosable"/>
         </div>
     </div>
 </template>
 <script setup lang="ts">
 import type {ResultListWidget } from '~/sdk';
+import RankedChoosable from './RankedChoosable.vue';
 
 
 interface WidgetProps {
