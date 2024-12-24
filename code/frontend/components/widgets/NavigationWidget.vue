e615ec794579c22cd486706756d8ede811a545d8
cmllr <mail@chmr.eu>
2024-12-24 10:48:22 +0100

    Add navigation

--- code/frontend/components/widgets/NavigationWidget.vue
+++ code/frontend/components/widgets/NavigationWidget.vue
@@ -1,16 +1,33 @@
 <template>
-    <div>
-        NAV NAV NAV
-    </div>
+  <div>
+    <a v-if="canGoBack" href="#" v-on:click="goBack"> Back </a>
+    <a v-if="canGoNext" href="#" v-on:click="goNext"> Skip </a>
+    <a v-if="canGoNext" href="#" v-on:click="goNext"> Forward </a>
+  </div>
 </template>
 <script setup lang="ts">
-import type { NavigationWidget } from '~/sdk';
-
+import type { NavigationWidget } from "~/sdk";
+import { useSessionStore } from "../../states/session";
 
 interface WidgetProps {
   widget: NavigationWidget;
 }
 
 const props = defineProps<WidgetProps>();
-
+const store = useSessionStore();
+const canGoNext = computed(() => {
+  return store.currentPage.nextPage != null;
+});
+const canGoBack = computed(() => {
+  const index = store.pages.findIndex((p) => p.id == store.currentPage.id);
+  return index > 0;
+});
+const goBack = () =>
+  store.selectPage(
+    store.pages[store.pages.findIndex((p) => p.id == store.currentPage.id) - 1].id
+  );
+const goNext = () =>
+  store.selectPage(
+    store.pages[store.pages.findIndex((p) => p.id == store.currentPage.id) +1].id
+  );
 </script>
