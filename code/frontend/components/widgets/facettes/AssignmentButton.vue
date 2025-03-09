457acdc5c2c443bac85fca5839389f2749a53c85
cmllr <mail@chmr.eu>
2025-03-09 15:40:35 +0100

    Make assignment buttons less attention seeking

--- code/frontend/components/widgets/facettes/AssignmentButton.vue
+++ code/frontend/components/widgets/facettes/AssignmentButton.vue
@@ -1,5 +1,5 @@
 <template>
-      <span><Icon role="button" class="align-middle ms-1" name="ion:code-slash-sharp" v-on:click="props.clickHandler"></Icon></span>
+      <span><Icon role="button" class="align-middle ms-1 assignment-button" name="ion:code-slash-sharp" v-on:click="props.clickHandler"></Icon></span>
 
 </template>
 <script setup lang="ts">
@@ -9,3 +9,12 @@ interface AssignmentButtonProps {
 const props = defineProps<AssignmentButtonProps>();
 
 </script>
+
+<style lang="scss" scoped>
+@import "../../../style/variables.scss";
+
+.assignment-button {
+  font-size: small;
+  color: $secondary;
+}
+</style>
