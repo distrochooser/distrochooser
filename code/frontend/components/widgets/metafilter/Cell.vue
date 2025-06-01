6a307e54e5acf4bfadd1caf4eb50341d948c02ef
cmllr <mail@chmr.eu>
2025-06-01 10:11:06 +0200

    Fix checkbox values not being removed

--- code/frontend/components/widgets/metafilter/Cell.vue
+++ code/frontend/components/widgets/metafilter/Cell.vue
@@ -8,14 +8,14 @@
             </label>
             <input  :value="formValue"
             class="form-control" :type="type" :id="id"
-            v-on:change="updateValue($event)">
+            v-on:change="updateValue($event, type)">
         </div>
 
         <div v-else class="form-check">   
             <label class="form-check-label" :for="id">
                 <LanguageTranslation :translation-key="id" />
             </label>
-            <input v-on:change="updateValue($event)" :checked="formValue.length > 0" class="form-check-input"
+            <input v-on:change="updateValue($event, type)" :checked="formValue.length > 0" class="form-check-input"
                 :type="type" :id="id">
          
         </div>
@@ -24,31 +24,51 @@
 <script lang="ts" setup>
 import { useState } from 'nuxt/app';
 import { useSessionStore } from '../../../states/session';
-import { computed } from 'vue';
-
-
+import { computed, type ComputedRef } from 'vue';
 
 interface MetaFilterCellProps {
     cellString: string;
 }
+
+enum MetaFilterType {
+    number = "number",
+    radio = "radio",
+    checkbox = "checkbox"
+}
+
 const props = defineProps<MetaFilterCellProps>();
 
-const type = computed(() => props.cellString.split('.')[0])
-const id = computed(() => props.cellString.split('.')[1])
-
+const type: ComputedRef<MetaFilterType>  = computed<MetaFilterType>(() => MetaFilterType[props.cellString.split('.')[0]])
+const id: ComputedRef<string> = computed(() => props.cellString.split('.')[1])
 const formValue = useState(props.cellString, () => "")
 
-// TODO: Add checkbox support
+const getValue = (el: HTMLInputElement, type: MetaFilterType) : string => {
+    return type == MetaFilterType.number ? el.value : "" + el.checked;
+}
 const store = useSessionStore();
 
-
-const updateValue = (e: Event) => {
+const updateValue = (e: Event, type: MetaFilterType) => {
     const el = (e.target as HTMLInputElement)
-    let value = ["radio", "checkbox"].indexOf(el.getAttribute("type")) == -1 ? el.value : "" + el.checked;
+    let value = getValue(el, type)
 
     const key = el.getAttribute("id")
-    if ((value.length) == 0) {
-        const oldId = store.metaValues.filter(m => m.key == key).map(m => m.id)
+    const oldId = store.metaValues.filter(m => m.key == key).map(m => m.id)
+    /**
+     * In case of checkboxes or radio buttons, in case the value is not true -> remove it from store entirely
+     */
+    if (type == MetaFilterType.checkbox || type  == MetaFilterType.radio) {
+        const boolValue = value == "true";
+        if (!boolValue){
+            store.removeMetaFilterArg(oldId)
+            return
+        }
+    }
+
+    /** 
+     * Fallback for non-checkbox/ radio elements, just update the value, if any. 
+     * Otherwise, get rid of it entirely
+     */
+    if (value.length == 0) {
         console.log(oldId)
         store.removeMetaFilterArg(oldId)
         formValue.value = ""
