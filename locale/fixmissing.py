717f668c95c2067c6b8e658358b7f238e26c5b07
cmllr <mail@chmr.eu>
2025-07-04 10:51:00 +0200

    Update ita translation to current state

---
+++ locale/fixmissing.py
@@ -0,0 +1,33 @@
+from json import dumps, loads
+from sys import argv
+from pyperclip import copy
+
+file = "./lang-eng.json"
+base_translation = loads(open(file, "r").read())
+
+
+lang_code = argv[1]
+file = f"./lang-{lang_code}.json"
+target_translation = loads(open(file, "r").read())
+
+
+for key in base_translation:
+    try:
+        value = target_translation[key]
+        if "!!" in value:
+            input_value = value.replace('!!', '').replace("distrochooser", "ABC").replace("Distrochooser", "ABC")
+            copy(input_value)
+            got = input(f"Get value for: {key}: {input_value}: ")
+            if "ABC" in got:
+                got = got.replace("ABC", "Distrochooser")
+            if got:
+                target_translation[key] = got
+    except KeyboardInterrupt:
+        break
+
+with open(file, "w") as file:
+    file.write(
+        dumps(target_translation, ensure_ascii=False, indent=4)
+        .encode("utf-8")
+        .decode()
+    )
