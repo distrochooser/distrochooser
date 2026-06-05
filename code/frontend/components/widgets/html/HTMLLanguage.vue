c3737c150b1b81b81c0390a004e8561c2be3d66d
cmllr <mail@chmr.eu>
2026-06-05 09:34:09 +0200

    Don't display translation hints if not in translation mode

--- code/frontend/components/widgets/html/HTMLLanguage.vue
+++ code/frontend/components/widgets/html/HTMLLanguage.vue
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 <template>
-    <p class="d-inline-flex gap-1" v-if="sessionStore.missingLanguageValues.length > 0">
+    <p class="d-inline-flex gap-1" v-if="sessionStore.missingLanguageValues.length > 0 && sessionStore.isTranslating">
         <a class="btn btn-primary" data-bs-toggle="collapse" href="#missingLanguageList" role="button" aria-expanded="false"
             aria-controls="missingLanguageList">
             
